81ca43c8155ca88cc6f783f59421770dd369feff
saratnayak0808 <152516593+saratnayak0808@users.noreply.github.com>
2024-08-22 02:49:48 +0530

    The End

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const cursor = new MouseFollower({
+  el: null,
+  container: document.body,
+  className: 'mf-cursor',
+  innerClassName: 'mf-cursor-inner',
+  textClassName: 'mf-cursor-text',
+  mediaClassName: 'mf-cursor-media',
+  mediaBoxClassName: 'mf-cursor-media-box',
+  iconSvgClassName: 'mf-svgsprite',
+  iconSvgNamePrefix: '-',
+  iconSvgSrc: '',
+  dataAttr: 'cursor',
+  hiddenState: '-hidden',
+  textState: '-text',
+  iconState: '-icon',
+  activeState: '-active',
+  mediaState: '-media',
+  stateDetection: {
+      '-pointer': 'a,button',
+      '-hidden': 'iframe'
+  },
+  visible: true,
+  visibleOnState: false,
+  speed: 0.55,
+  ease: 'expo.out',
+  overwrite: true,
+  skewing: 0,
+  skewingText: 2,
+  skewingIcon: 2,
+  skewingMedia: 2,
+  skewingDelta: 0.001,
+  skewingDeltaMax: 0.15,
+  stickDelta: 0.15,
+  showTimeout: 20,
+  hideOnLeave: true,
+  hideTimeout: 300,
+  hideMediaTimeout: 300
+});
+
+
+
 
 let todos = [];
 
@@ -7,7 +48,8 @@ function addTodo() {
 
     if (todoTitle !== '') {
         todos.push({
-            title: todoTitle
+            title: todoTitle,
+            completed: false
         });
         document.querySelector("#inp-text").value = '';
          render();
@@ -40,13 +82,28 @@ function tdone(index){
     edited.value = edited.value;
     edited.setAttribute("readonly", "readonly");
    }
+}
+
+function strik(index){
+  const checkBox = document.querySelector(`#check-box${index}`);
+  const taskText = document.querySelector(`#taskInputEdit${index}`);
+
+ todos[index].completed =!todos[index].completed;
   
+ if (checkBox.checked) {
+  taskText.classList.add('completed');
+ } else {
+  taskText.classList.remove('completed');
  }
+
+}
+
 
 function createTodoComponent(todos,index){
    
   const div = document.createElement("div")
    div.innerHTML=`<div class="Tasks">
+    <input type="checkbox" id="check-box${index}" onchange="strik(${index})" ${todos.completed ? "checked" : ""} value="${todos.title}"/>
         <input type="text" id="taskInputEdit${index}" value="${todos.title}" readonly>
         <button onclick="tdone(${index})"><img src="assets/check-line (1).svg" alt=""></button>
         <button onclick="taskedit(${index})"><img src="assets/pencil.svg" alt=""></button>
@@ -65,6 +122,9 @@ function render(){
 }
 
 
+
+
+//animation part
 
 
 
